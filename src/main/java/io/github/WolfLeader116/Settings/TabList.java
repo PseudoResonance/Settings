dfe6e2c179267c780313b4a1623c603ee9c7a6e5
WolfLeader116 <kaio11602@gmail.com>
2015-04-25 19:02:34 -0700

    Update 1.2.3

--- src/main/java/io/github/WolfLeader116/Settings/TabList.java
+++ src/main/java/io/github/WolfLeader116/Settings/TabList.java
@@ -2,8 +2,6 @@ package io.github.WolfLeader116.Settings;
 
 import io.github.wolfleader116.chat.ChatPlugin;
 
-import java.util.ArrayList;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.mcsg.double0negative.tabapi.TabAPI;
@@ -11,17 +9,12 @@ import org.mcsg.double0negative.tabapi.TabAPI;
 public class TabList {
 	
 	public static void tablist() {
-		ArrayList<String> players = new ArrayList<String>();
-		for (Player pl : Bukkit.getServer().getOnlinePlayers()) {
-			String name = pl.getName();
-			players.add(name);
-			
-		}
 		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
 			TabAPI.setPriority(Settings.plugin, p, 0);
+			TabAPI.updatePlayer(p);
 			TabAPI.setTabString(Settings.plugin, p, 0, 0, "§2----------" + TabAPI.nextNull(), 0);
-			TabAPI.setTabString(Settings.plugin, p, 0, 1, "§2---------- + TabAPI.nextNull()", 0);
-			TabAPI.setTabString(Settings.plugin, p, 0, 2, "§2---------- + TabAPI.nextNull()", 0);
+			TabAPI.setTabString(Settings.plugin, p, 0, 1, "§2----------" + TabAPI.nextNull(), 0);
+			TabAPI.setTabString(Settings.plugin, p, 0, 2, "§2----------" + TabAPI.nextNull(), 0);
 			TabAPI.setTabString(Settings.plugin, p, 1, 0, "§6Online" + TabAPI.nextNull(), 5);
 			TabAPI.setTabString(Settings.plugin, p, 1, 1, Settings.plugin.getConfig().getString("ServerName") + TabAPI.nextNull(), 5);
 			TabAPI.setTabString(Settings.plugin, p, 1, 2, "§6Party" + TabAPI.nextNull(), 5);
@@ -30,13 +23,13 @@ public class TabList {
 			TabAPI.setTabString(Settings.plugin, p, 2, 2, "§2----------" + TabAPI.nextNull(), 0);
 			int down = 3;
 			int across = 0;
-			for (String name : players) {
+			for (Player pl : Bukkit.getServer().getOnlinePlayers()) {
 				if (across == 2) {
 					across = 0;
 					down++;
 				}
 				if (down != 20) {
-					TabAPI.setTabString(Settings.plugin, p, down, across, ChatPlugin.getNick(Bukkit.getServer().getPlayer(name)) + TabAPI.nextNull(), 3);
+					TabAPI.setTabString(Settings.plugin, p, down, across, ChatPlugin.getNick(pl) + TabAPI.nextNull(), 3);
 					across++;
 				}
 			}
